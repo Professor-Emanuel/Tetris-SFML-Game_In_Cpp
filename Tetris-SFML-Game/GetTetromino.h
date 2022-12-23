7f4f1408f34595b1375969523b9fe828df3771c8
Professor-Emanuel <87651738+Professor-Emanuel@users.noreply.github.com>
2022-12-23 16:11:34 +0200

    Add files via upload

---
+++ Tetris-SFML-Game/GetTetromino.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+#include "Global.h"
+
+std::vector<Position> get_tetromino(unsigned char i_shape, unsigned char i_x, unsigned char i_y);
---
+++ Tetris-SFML-Game/GetWallKickData.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+#include "Global.h"
+
+std::vector<Position> get_wall_kick_data(bool i_is_i_shape, unsigned char i_current_rotation, unsigned char i_next_rotation);
